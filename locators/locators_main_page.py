a95c20c3124b9014810f185bf19736e65efdc429
nikrud <89258843+nikrud@users.noreply.github.com>
2022-09-09 17:50:50 +0300

    add locators

--- locators/locators_main_page.py
+++ locators/locators_main_page.py
@@ -14,10 +14,15 @@ class MainPagelocators:
     PARIS_TREND_REPORT_BUTTON = (By.XPATH, '//*[@id="hm-46b73c23-10b6-32ea-b7ee-4f6cc6c447dchm-4b775b60-96bd-3157-96eb-b106549e062d"]/div/article/div/div/div/div/ul/li/a')
     PARIS_TREND_REPORT_PAGE_IS_OPEN = (By.XPATH, '//*[@id="text-01"]/div/h1')
     TURN_UP_THE_COLOR_BUTTON = (By.XPATH, '//*[@id="personalizedteaser-segmented-teaser"]/div/article/div/div/div/div/ul/li/a')
-    TURN_UP_THE_COLOR_PAGE_IS_OPEN = (By.XPATH, '//*[@id="page-content"]/div/div[1]/h1')
+    TURN_UP_THE_COLOR_PAGE_IS_OPEN = (By.XPATH, '// *[ @ id = "text-01"] / div / h1')
+    VIEW_ALL_BUTTON = (By.XPATH, '//*[@id="menu-links"]/li[1]/ul/li[1]/a')
+    MAXIMUM = (By.XPATH, '//*[@id="page-content"]/div/div[3]/div[2]/h2')
+    LOAD_MORE_PRODUCTS_BUTTON = (By.XPATH, '//*[@id="page-content"]/div/div[3]/div[2]/button')
 class SocialNetworkLocators:
     FACEBOOK_PAGE_BUTTON = (By.XPATH, '/html/body/footer/div/ul/li[1]/a')
     TWITTER_PAGE_BUTTON = (By.XPATH, '/html/body/footer/div/ul/li[2]/a')
     INSTAGRAM_PAGE_BUTTON = (By.XPATH, '/html/body/footer/div/ul/li[3]/a')
     YOUTUBE_PAGE_BUTTON = (By.XPATH, '/html/body/footer/div/ul/li[4]/a')
     PINTEREST_PAGE_BUTTON = (By.XPATH, '/html/body/footer/div/ul/li[5]/a')
+
+
