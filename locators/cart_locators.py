1c4ef214b6d8e6a4fd8c4c10491092e5f27723a8
nikrud <89258843+nikrud@users.noreply.github.com>
2022-09-15 11:05:40 +0300

    Add new locators

--- locators/cart_locators.py
+++ locators/cart_locators.py
@@ -8,6 +8,7 @@ class ProductPageLocators:
     SELECT_SIZE = (By.XPATH, '//*[@id="picker-1"]/button')
     ITEM_IN_STOCK = (By.CSS_SELECTOR, "li[class='item  js-enable-nib']")
     CART_PAGE_BUTTON = (By.XPATH, '/html/body/header/nav/ul[1]/li[4]/span/a')
+    FAVOURITE_BUTTON = (By.XPATH, '//*[@id="main-content"]/div[1]/div[2]/div[1]/div[1]/div/hm-favourites-button/button')
 
 class CartLocators:
     PRODUCT_NAME_IN_CART = (By.XPATH, '//*[@id="sidebar-sticky-boundary"]/section[1]/div/ul/li/article/div[1]/h2')
