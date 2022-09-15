2a727ad40387fffc5f9b0a53de3a64d434dbdf86
nikrud <89258843+nikrud@users.noreply.github.com>
2022-09-15 10:43:26 +0300

    add option

--- conftest.py
+++ conftest.py
@@ -2,13 +2,17 @@ import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 import time
-
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope="function")
 def browser():
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+    chrome_options = Options()
+    chrome_options.add_argument("--disable-infobars")
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
     driver.implicitly_wait(5)
     yield driver
     time.sleep(20)
     driver.quit()
+
+
